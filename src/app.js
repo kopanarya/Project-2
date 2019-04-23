3e72ff7f45fa0824b70f5eb17795ba5221e509f0
Elle Whaley <elliewhaley11@gmail.com>
2019-04-23 10:08:08 +0100

    Added setup

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {HashRouter as Router ,Route,Switch} from 'react-router-dom'
+import 'bulma'
+
+class App extends React.Components{
+
+  render(){
+    return ()
+
+  }
+}
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Home =()=>{
+
+  return(
+    <section className="section">
+      <div className="container">
+        <h1 className="title is-1">London Daily</h1>]
+        <h1 className="subtitle is-3">Site for Londoners</h1>
+
+      </div>
+    </section>
+  )
+}
